61312660347861a947643cc65c465fcebd043084
alphadev <it.dev.unity1102@gmail.com>
2023-10-12 15:41:02 -0700

    main

---
+++ Pong/Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Steamworks;
+using System;
+using System.Diagnostics;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private bool isSteamRunning=false;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            Debug.WriteLine("SteamAPI.Init()!");
+            try
+            {
+                if (!SteamAPI.Init())
+                {
+                    Debug.WriteLine("SteamAPI.Init() failed!");
+                    return;
+                }
+                else
+                {
+                    isSteamRunning = true;
+                }
+            }
+            catch (DllNotFoundException e)
+            { // We check this here as it will be the first instance of it.
+                Debug.WriteLine(e);
+                return;
+            }
+
+
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
